da9981fcfe7e689033b1c4e613c6eda22172121d
Dzmitry Malchanau <dzmitry_malchanau@epam.com>
2017-09-26 13:42:23 +0300

    ds

--- src/main/java/practice/framework/resources/FramerorkProperties.java
+++ src/main/java/practice/framework/resources/FramerorkProperties.java
@@ -8,7 +8,7 @@ public class FramerorkProperties {
 	public String serverURL;
 	public String testNGFile;
 	public String browserName;
-
+    //насколько целесообразно инициализировать все проперти? - можно ли простов ызывать их по мере надобности из класса PropertiesLoader?
 	public void initiateProperties() {
 		this.serverURL = PropertiesLoader.getProperty("serverURL");//replace with the constant
 		this.testNGFile = PropertiesLoader.getProperty("nestNGFilePAth");//replace with the constant
